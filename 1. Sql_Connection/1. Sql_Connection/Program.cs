ae30ff95864c055874a1dd7ab2409a79332c98a9
OnkarM09 <onkarmeherwade@gmail.com>
2025-05-24 22:38:05 +0530

    Added sqlconnection

---
+++ 1. Sql_Connection/1. Sql_Connection/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+namespace _1._Sql_Connection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.GetConnection();
+        }
+
+
+        public static void GetConnection()
+        {
+
+            string connectionString = "Data Source=LAPTOP-01OFR6OL; Initial Catalog=ado_db; Integrated Security=true; Encrypt=false;";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            connection.Open();
+            if (connection.State == ConnectionState.Open)
+            {
+                Console.WriteLine("Connection is open");
+            }
+            connection.Close();
+            Console.WriteLine("Connection is closed now");
+        }
+    }
+}
